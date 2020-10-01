f5c597db82ae320147f20b4ef99a0c581db9cb97
LAK132 <LAK132@kicodora.com>
2020-10-01 23:04:18 +0930

    clang-format

--- examples/imgui_impl_softraster.cpp
+++ examples/imgui_impl_softraster.cpp
@@ -5,57 +5,61 @@ texture_base_t *Screen = nullptr;
 
 bool ImGui_ImplSoftraster_Init(texture_base_t *screen)
 {
-    if (screen != nullptr)
-    {
-        Screen = screen;
-        return true;
-    }
-    return false;
+  if (screen != nullptr)
+  {
+    Screen = screen;
+    return true;
+  }
+  return false;
 }
 
 void ImGui_ImplSoftraster_Shutdown()
 {
-    Screen = nullptr;
+  Screen = nullptr;
 }
 
 void ImGui_ImplSoftraster_NewFrame()
 {
-    if (Screen == nullptr) return;
+  if (Screen == nullptr) return;
 
-    ImGuiIO& io = ImGui::GetIO();
-    io.DisplaySize.x = Screen->w;
-    io.DisplaySize.y = Screen->h;
+  ImGuiIO &io      = ImGui::GetIO();
+  io.DisplaySize.x = Screen->w;
+  io.DisplaySize.y = Screen->h;
 }
 
-void ImGui_ImplSoftraster_RenderDrawData(ImDrawData* draw_data)
+void ImGui_ImplSoftraster_RenderDrawData(ImDrawData *draw_data)
 {
-    if (Screen == nullptr) return;
+  if (Screen == nullptr) return;
 
-    Screen->clear();
+  Screen->clear();
 
-    switch (Screen->type)
-    {
+  switch (Screen->type)
+  {
     case texture_type_t::ALPHA8:
-        renderDrawLists<int32_t>(draw_data, *reinterpret_cast<texture_alpha8_t*>(Screen));
-        break;
+      renderDrawLists<int32_t>(draw_data,
+                               *reinterpret_cast<texture_alpha8_t *>(Screen));
+      break;
 
     case texture_type_t::VALUE8:
-        renderDrawLists<int32_t>(draw_data, *reinterpret_cast<texture_value8_t*>(Screen));
-        break;
+      renderDrawLists<int32_t>(draw_data,
+                               *reinterpret_cast<texture_value8_t *>(Screen));
+      break;
 
     case texture_type_t::COLOR16:
-        renderDrawLists<int32_t>(draw_data, *reinterpret_cast<texture_color16_t*>(Screen));
-        break;
+      renderDrawLists<int32_t>(draw_data,
+                               *reinterpret_cast<texture_color16_t *>(Screen));
+      break;
 
     case texture_type_t::COLOR24:
-        renderDrawLists<int32_t>(draw_data, *reinterpret_cast<texture_color24_t*>(Screen));
-        break;
+      renderDrawLists<int32_t>(draw_data,
+                               *reinterpret_cast<texture_color24_t *>(Screen));
+      break;
 
     case texture_type_t::COLOR32:
-        renderDrawLists<int32_t>(draw_data, *reinterpret_cast<texture_color32_t*>(Screen));
-        break;
+      renderDrawLists<int32_t>(draw_data,
+                               *reinterpret_cast<texture_color32_t *>(Screen));
+      break;
 
     default: return;
-    }
+  }
 }
-
--- misc/softraster/defines.h
+++ misc/softraster/defines.h
@@ -2,12 +2,14 @@
 #define SOFTRASTER_DEFINES_H
 
 #ifdef ARDUINO
-#   include "Arduino.h"
-#   define INLINE_DEC(F) inline F __attribute__((always_inline))
+#  include "Arduino.h"
+#  define INLINE_DEC(F) inline F __attribute__((always_inline))
 #else
-#   define INLINE_DEC(F) inline F
+#  define INLINE_DEC(F) inline F
 #endif
 #define INLINE_DEF(F) inline F
-#define INLINE(F) INLINE_DEC(F); INLINE_DEF(F)
+#define INLINE(F)                                                             \
+  INLINE_DEC(F);                                                              \
+  INLINE_DEF(F)
 
 #endif // SOFTRASTER_DEFINES_H
